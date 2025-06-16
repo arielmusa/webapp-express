b5eb6a9574836b22d4b7b44bd9a87328c7409567
arielmusa <arielmusainfo@gmail.com>
2025-06-16 19:37:11 +0200

    feat: get also reviews from SHOW API call

--- routers/movies.js
+++ routers/movies.js
@@ -4,9 +4,8 @@ import { connection } from "../data/db.js";
 
 const router = express.Router();
 
-const { APP_PORT, APP_URI } = process.env;
-
-const url = `${APP_URI}${APP_PORT ? "" + APP_PORT : ""}`;
+/* const { APP_PORT, APP_URI } = process.env;
+const url = `${APP_URI}${APP_PORT ? "" + APP_PORT : ""}`; */
 
 router.get("", (req, res) => {
   const sql = `SELECT * FROM movies`;
@@ -18,16 +17,25 @@ router.get("", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  const sql = `SELECT * FROM movies WHERE id = ?`;
+  const sqlMovies = `SELECT * FROM movies WHERE id = ?`;
+  const sqlReviews = `SELECT * FROM reviews WHERE movie_id = ?`;
 
-  connection.query(sql, [id], (err, results) => {
+  connection.query(sqlMovies, [id], (err, movieResults) => {
     if (err)
       return res
         .status(500)
         .json({ error: 500, message: "Database query failed" });
-    if (!results.length)
+    if (!movieResults.length)
       return res.status(404).json({ error: 404, message: "item not found" });
-    res.json(results);
+    connection.query(sqlReviews, [id], (err, reviewsResults) => {
+      if (err)
+        res.status(500).json({ error: 500, message: "Database query failed" });
+
+      res.json({
+        movie: movieResults,
+        reviews: reviewsResults,
+      });
+    });
   });
 });
 
