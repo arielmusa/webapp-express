c4c914d8020fd31c994cd0e7ea61d847930ceea0
arielmusa <arielmusainfo@gmail.com>
2025-06-17 16:26:14 +0200

    feat(app.js): add cors middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import "./data/db.js";
+import cors from "cors";
 import { router } from "./routers/moviesRouter.js";
 const app = express();
 
@@ -8,6 +9,13 @@ const { APP_PORT, APP_URI } = process.env;
 
 const url = `${APP_URI}${APP_PORT ? ":" + APP_PORT : ""}`;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.json());
 app.use("/movies", router);
 
 app.listen(3000, () => {
